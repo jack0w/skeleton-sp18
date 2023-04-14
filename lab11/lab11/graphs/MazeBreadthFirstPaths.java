dec6d6018803bc785a27430d32d9f1cbe1df8c6a
jack0w <cwang82@u.rochester.edu>
2023-04-13 20:27:48 -0400

    Debug: marked the source

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -24,6 +24,7 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        marked[s] = true;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
